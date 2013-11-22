e5bf7da5459eaa17d2e593b18bcde0eeee44f3b5
Matt Callaway <mcallawa@genome.wustl.edu>
2013-11-22 15:19:00 -0600

    Add our init scripts to specify nice value

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,6 +7,7 @@ class ssh::params {
       $ssh_config = '/etc/ssh/ssh_config'
       $ssh_known_hosts = '/etc/ssh/ssh_known_hosts'
       $service_name = 'ssh'
+      $init_script_name = "${service_name}.${::lsbdistcodename}"
     }
     redhat: {
       $server_package_name = 'openssh-server'
@@ -15,6 +16,7 @@ class ssh::params {
       $ssh_config = '/etc/ssh/ssh_config'
       $ssh_known_hosts = '/etc/ssh/ssh_known_hosts'
       $service_name = 'sshd'
+      $init_script_name = "${service_name}.el${::operatingsystemmajrelease}"
     }
     default: {
       case $::operatingsystem {
--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -3,4 +3,8 @@ class ssh::server::install {
   package { $ssh::params::server_package_name:
     ensure => present,
   }
+  file { $ssh::params::service_name:
+    ensure => present,
+    source => "puppet:///modules/${module_name}/${init_script_name}"
+  }
 }
