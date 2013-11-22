7a090450879e9d03d72572ed60f852b2cbd104f1
Matt Callaway <mcallawa@genome.wustl.edu>
2013-11-22 15:29:54 -0600

    Fully qualify path

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -3,8 +3,11 @@ class ssh::server::install {
   package { $ssh::params::server_package_name:
     ensure => present,
   }
-  file { $ssh::params::service_name:
+  file { "/etc/init.d/${ssh::params::service_name}":
     ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
     source => "puppet:///modules/${module_name}/${init_script_name}"
   }
 }
